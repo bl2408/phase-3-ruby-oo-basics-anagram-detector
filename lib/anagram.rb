e88eac31e4fd0c074fbedae3d387ca22b2a35818
Brian Lambert <brianlambert2408@gmail.com>
2022-11-08 10:00:56 +1100

    Completed tasks

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize word
+        @word = word
+    end
+
+    def match ws
+        match = []
+        ws.each do | w |
+            match << w if @word.chars.sort == w.chars.sort
+        end
+        match
+    end
+end
